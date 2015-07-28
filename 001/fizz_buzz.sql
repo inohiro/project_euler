4b6f0b9a0028310187f7a34035b88740f8d8e8fb
Hiroyuki Inoue <hiroyuki-inoue@cookpad.com>
2015-07-28 18:59:55 +0900

    Add 001

---
+++ 001/fizz_buzz.sql
@@ -0,0 +1,12 @@
+select
+    sum(a.generate_series)
+from (
+    select
+        *
+    from
+        generate_series(0, 1000)
+) a
+where
+    a.generate_series % 3 = 0
+    or a.generate_series % 5 = 0
+;
